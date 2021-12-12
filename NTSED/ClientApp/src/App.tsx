e45a615f16d416911ca28cd066e8da82d06a4ac7
Karolis2011 <Karolis2011@users.noreply.github.com>
2021-12-12 21:49:17 +0200

    Added front end app on to witch to add things.

---
+++ NTSED/ClientApp/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Todo: show some things</h1>
+    </div>
+  );
+}
+
+export default App;
--- NTSED/Program.cs
+++ NTSED/Program.cs
@@ -1,6 +1,20 @@
+using Microsoft.AspNetCore.Rewrite;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/clear", () => 0);
+app.UseStaticFiles();
+
+var rewriteOptions = new RewriteOptions()
+    .AddRewrite("^ntsed/(.*)", "$1", false);
+app.UseRewriter(rewriteOptions);
+
+app.MapGet("/clear", int () => throw new NotImplementedException());
+app.MapGet("/new_program", int () => throw new NotImplementedException());
+app.MapGet("/execute", int () => throw new NotImplementedException());
+app.MapGet("/remove", int () => throw new NotImplementedException());
+app.MapGet("/computer/get_buffer", string () => throw new NotImplementedException());
+app.MapGet("/computer/topic", int () => throw new NotImplementedException());
+
 
 app.Run();
